1e30a394f04fce74210fd070287c9316552de373
Rwondaz <81451616+rwondaz@users.noreply.github.com>
2021-04-04 02:52:16 +0100

    Update chart-area-demo.js

--- chart-area-demo.js
+++ chart-area-demo.js
@@ -39,7 +39,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 20,
+          max: 40,
           maxTicksLimit: 5
         },
         gridLines: {
