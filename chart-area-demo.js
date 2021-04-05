47f7c9cbcd71aa67e8812e273b63499d3838ad70
Rwondaz <81451616+rwondaz@users.noreply.github.com>
2021-04-05 02:30:07 +0100

    Update chart-area-demo.js

--- chart-area-demo.js
+++ chart-area-demo.js
@@ -7,7 +7,7 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: [ "Mar 9 @ 11:59 PM", "Mar 10 @ 11:59 PM", "Mar 11 @ 11:59 PM", "Mar 12 @ 11:59 PM", "Mar 13 @ 11:59 PM", "Mar 14 @ 11:59 PM", "Mar 15 @ 11:59 PM", "Mar 16 @ 11:59 PM", "Mar 17 @ 11:59 PM", "Mar 18 @ 11:59 PM", "Mar 19 @ 11:59 PM", "Mar 20 @ 11:59 PM", "Mar 21 @ 11:59 PM", "Mar 22 @ 11:59 PM ", "Mar 23 @ 11:59 PM", "Mar 24 @ 11:59 PM", "Mar 25 @ 11:59 PM", "Mar 26 @ 11:59 PM", "Mar 27 @ 11:59 PM", "Mar 28 @ 11:59 PM", "Mar 29 @ 11:59 PM", "Mar 30 @ 11:59 PM", "Mar 31 @ 11:59 PM", "Apr 1 @ 11:59 PM", "Apr 2 @ 11:59 PM", "Apr 3 @ 11:59 PM",    ],
+    labels: [ "Mar 9 @ 11:59 PM", "Mar 10 @ 11:59 PM", "Mar 11 @ 11:59 PM", "Mar 12 @ 11:59 PM", "Mar 13 @ 11:59 PM", "Mar 14 @ 11:59 PM", "Mar 15 @ 11:59 PM", "Mar 16 @ 11:59 PM", "Mar 17 @ 11:59 PM", "Mar 18 @ 11:59 PM", "Mar 19 @ 11:59 PM", "Mar 20 @ 11:59 PM", "Mar 21 @ 11:59 PM", "Mar 22 @ 11:59 PM ", "Mar 23 @ 11:59 PM", "Mar 24 @ 11:59 PM", "Mar 25 @ 11:59 PM", "Mar 26 @ 11:59 PM", "Mar 27 @ 11:59 PM", "Mar 28 @ 11:59 PM", "Mar 29 @ 11:59 PM", "Mar 30 @ 11:59 PM", "Mar 31 @ 11:59 PM", "Apr 1 @ 11:59 PM", "Apr 2 @ 11:59 PM", "Apr 3 @ 11:59 PM", Apr 4 @ 11:59 PM   ],
     datasets: [{
       label: "Price",
       lineTension: 0.3,
@@ -20,7 +20,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [0.05, 0.05, 0.05, 0.06, 0.05, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.06, 0.06, 0.07, 0.07, 0.07, 0.07, 0.07, 0.27, 10.59, 23.03, 40.30, ],
+      data: [0.05, 0.05, 0.05, 0.06, 0.05, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.06, 0.06, 0.07, 0.07, 0.07, 0.07, 0.07, 0.27, 10.59, 23.03, 40.30, 35.45 ],
     }],
   },
   options: {
